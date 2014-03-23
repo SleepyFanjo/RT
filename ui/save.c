f3fc4880ee03d80f77fa8b4fb9a032e78ca8c2a7
lredoban <lredoban@student.42.fr>
2014-03-23 15:08:09 +0100

    petite remise a la norme de save.c

--- ui/save.c
+++ ui/save.c
@@ -12,11 +12,11 @@ void		put_cam(t_param *param, int fd)
 
 static void lil_put_sphere(t_sphere *s, int fd)
 {
-	l_printf("%q%d %d %d %d ",fd, (int)s->pos.x, (int)s->pos.y, (int)s->pos.z
+	l_printf("%q%d %d %d %d ", fd, (int)s->pos.x, (int)s->pos.y, (int)s->pos.z
 		, s->radius);
-	l_printf("%q%d %d %d %d %d %d ",fd, (int)s->lim_h_x, (int)s->lim_b_x
+	l_printf("%q%d %d %d %d %d %d ", fd, (int)s->lim_h_x, (int)s->lim_b_x
 		, (int)s->lim_h_y, (int)s->lim_b_y, (int)s->lim_h_z, (int)s->lim_b_z);
-	l_printf("%q%d %d %d %d ",fd, (int)(s->mat.shine * 100)
+	l_printf("%q%d %d %d %d ", fd, (int)(s->mat.shine * 100)
 			, (int)(s->mat.reflex * 100), (int)(s->mat.med_in * 100)
 			, (int)(s->mat.refrax * 100));
 	l_printf("%q%X%X%X\n", fd, s->color[2], s->color[1], s->color[0]);
@@ -42,7 +42,7 @@ static void lil_put_spot(t_spot *s, int fd)
 	int		value;
 
 	value = (int)(s->value * 1000);
-	l_printf("%q%d %d %d %d ",fd, (int)s->coord.x, (int)s->coord.y
+	l_printf("%q%d %d %d %d ", fd, (int)s->coord.x, (int)s->coord.y
 			, (int)s->coord.z, value);
 	l_printf("%q%X%X%X\n", fd, s->color[2], s->color[1], s->color[0]);
 }
@@ -64,9 +64,9 @@ void		put_spot(t_param *param, int fd)
 
 static void lil_put_plane(t_plane *p, int fd)
 {
-	l_printf("%q%d %d %d ",fd, (int)p->pos.x, (int)p->pos.y, (int)p->pos.z);
-	l_printf("%q%d %d %d ",fd, (int)p->vec.x, (int)p->vec.y, (int)p->vec.z);
-	l_printf("%q%d %d %d %d ",fd, (int)(p->mat.shine * 100)
+	l_printf("%q%d %d %d ", fd, (int)p->pos.x, (int)p->pos.y, (int)p->pos.z);
+	l_printf("%q%d %d %d ", fd, (int)p->vec.x, (int)p->vec.y, (int)p->vec.z);
+	l_printf("%q%d %d %d %d ", fd, (int)(p->mat.shine * 100)
 			, (int)(p->mat.reflex * 100), (int)(p->mat.med_in * 100)
 			, (int)(p->mat.refrax * 100));
 	l_printf("%q%X%X%X\n", fd, p->color[2], p->color[1], p->color[0]);
@@ -89,10 +89,10 @@ void		put_plane(t_param *param, int fd)
 
 static void lil_put_cone(t_cone *s, int fd)
 {
-	l_printf("%q%d %d %d ",fd, (int)s->pos.x, (int)s->pos.y, (int)s->pos.z);
-	l_printf("%q%d %d %d %d ",fd, (int)s->rot.x, (int)s->rot.y, (int)s->rot.z
+	l_printf("%q%d %d %d ", fd, (int)s->pos.x, (int)s->pos.y, (int)s->pos.z);
+	l_printf("%q%d %d %d %d ", fd, (int)s->rot.x, (int)s->rot.y, (int)s->rot.z
 			, s->alpha);
-	l_printf("%q%d %d %d %d ",fd, (int)(s->mat.shine * 100)
+	l_printf("%q%d %d %d %d ", fd, (int)(s->mat.shine * 100)
 			, (int)(s->mat.reflex * 100), (int)(s->mat.med_in * 100)
 			, (int)(s->mat.refrax * 100));
 	l_printf("%q%X%X%X\n", fd, s->color[2], s->color[1], s->color[0]);
@@ -115,10 +115,10 @@ void		put_cone(t_param *param, int fd)
 
 static void lil_put_cylinder(t_cylinder *s, int fd)
 {
-	l_printf("%q%d %d %d ",fd, (int)s->pos.x, (int)s->pos.y, (int)s->pos.z);
-	l_printf("%q%d %d %d %d ",fd, (int)s->rot.x, (int)s->rot.y, (int)s->rot.z
+	l_printf("%q%d %d %d ", fd, (int)s->pos.x, (int)s->pos.y, (int)s->pos.z);
+	l_printf("%q%d %d %d %d ", fd, (int)s->rot.x, (int)s->rot.y, (int)s->rot.z
 		, s->radius);
-	l_printf("%q%d %d %d %d ",fd, (int)(s->mat.shine * 100)
+	l_printf("%q%d %d %d %d ", fd, (int)(s->mat.shine * 100)
 			, (int)(s->mat.reflex * 100), (int)(s->mat.med_in * 100)
 			, (int)(s->mat.refrax * 100));
 	l_printf("%q%X%X%X\n", fd, s->color[2], s->color[1], s->color[0]);
