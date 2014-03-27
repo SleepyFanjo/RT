7030a3ac3c4ce8e85a109476cdd6f605635576c5
lredoban <lredoban@student.42.fr>
2014-03-27 16:05:03 +0100

    hook norm

--- server/hook.c
+++ server/hook.c
@@ -6,20 +6,20 @@
 /*   By: vwatrelo <vwatrelo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/27 12:03:38 by vwatrelo          #+#    #+#             */
-/*   Updated: 2014/03/27 12:03:39 by vwatrelo         ###   ########.fr       */
+/*   Updated: 2014/03/27 15:58:30 by lredoban         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/network.h"
 #include "../includes/ui.h"
 
-int		mouse_hook(int button, int x, int y, t_v_env *e)
+int			mouse_hook(int button, int x, int y, t_v_env *e)
 {
 	t_info		*info;
 	t_param		*param;
 	static void	(*obj_tab[4])(t_param *, t_info *) =
-		{&clic_sphere, &clic_plane, &clic_cylinder, &clic_cone};
 
+	{&clic_sphere, &clic_plane, &clic_cylinder, &clic_cone};
 	param = e->p;
 	(void)button;
 	calc_matrix((void *)param, CAM);
@@ -36,38 +36,19 @@ int		mouse_hook(int button, int x, int y, t_v_env *e)
 	return (0);
 }
 
-int		expose_hook(void *env)
+int			expose_hook(void *env)
 {
-		t_v_env		*e;
+	t_v_env		*e;
 
-		e = (t_v_env *)env;
-		if (e->img == NULL)
-			return (0);
-		mlx_put_image_to_window(e->mlx, e->win, e->img, 0, 0);
+	e = (t_v_env *)env;
+	if (e->img == NULL)
 		return (0);
+	mlx_put_image_to_window(e->mlx, e->win, e->img, 0, 0);
+	return (0);
 }
 
-int		key_hook(int keycode, t_v_env *e)
+static void	norminette_mary_me(t_param *param, t_v_env *e, int keycode, int i)
 {
-	int			i;
-	static int	(*key_tab[10])(int, t_param *) =
-	{
-		&arrows, &how_high, &wasd, &render, &live, &back_to_cam, &roll
-		, &rad, &del, &copy
-	};
-	t_param		*param;
-
-	param = e->p;
-	i = 0;
-	if (keycode == 65307)
-		exit(0);
-	R = 0;
-	while (i != 10)
-	{
-		if (key_tab[i](keycode, param))
-			break ;
-		i++;
-	}
 	if (i != 10)
 	{
 		save(param);
@@ -82,5 +63,27 @@ int		key_hook(int keycode, t_v_env *e)
 		ft_putnbr(keycode);
 		ft_putstr("\n");
 	}
+}
+
+int			key_hook(int keycode, t_v_env *e)
+{
+	int			i;
+	t_param		*param;
+	static int	(*key_tab[10])(int, t_param *) =
+
+	{&arrows, &how_high, &wasd, &render, &live, &back_to_cam, &roll
+	, &rad, &del, &copy};
+	param = e->p;
+	i = 0;
+	if (keycode == 65307)
+		exit(0);
+	R = 0;
+	while (i != 10)
+	{
+		if (key_tab[i](keycode, param))
+			break ;
+		i++;
+	}
+	norminette_mary_me(param, e, keycode, i);
 	return (0);
 }
